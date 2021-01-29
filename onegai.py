a6315c4b59923db2de48996b117dfcedd9ccacf9
Cyanos09 <deftera.monday@gmail.com>
2021-01-30 07:34:32 +0900

    commit

--- onegai.py
+++ onegai.py
@@ -1,41 +1,74 @@
 from urllib import request
 from bs4 import BeautifulSoup
 import re
-n = input("put a number: ")
 
-url = 'http://factordb.com/index.php?query='+str(n)
-print(url)
-response = request.urlopen(url)
-soup = BeautifulSoup(response,"lxml")
-response.close()
+def prepare():
+    n = input("put a number: ")
+    url = 'http://factordb.com/index.php?query='+str(n)
+    id = ""
+    print(url)
+    return url
 
-tables = soup.findAll("table")
-scr = []
-count = 0
-for table in tables:
-    for r in table.findAll('tr'):
-        if(count == 1):
-            d = r.findAll("td")
-            scr.append(str(d))
-    count+=1
+def scraping(url):
+    response = request.urlopen(url)
+    soup = BeautifulSoup(response,"lxml")
+    response.close()
+    return soup
 
-data = scr[2].split(",")
+def bigfactor(fac):
+    urltmp = 'http://factordb.com/index.php?showid='
+    id = re.search('id=.+"><',fac).group()
+    id = id.lstrip('id=')
+    id = id.rstrip('"><')
+    urltmp += str(id)
+    soup = scraping(urltmp)
+    tables = soup.findAll("table")
+    scr = find(tables)
+    data = scr[2].split(",")
+    print(data)
+    printfactor(data)
 
-factmp = ''
-for i,factor in enumerate(data):
-    if(i==2):
-        factmp = factor
-       
-faclist = []
-faclist = factmp.split("</a>")
-faclist2 = []
+    
+def find(tables):
+    scr = []
+    count = 0
+    for table in tables:
+        for r in table.findAll('tr'):
+            if(count == 1):
+                d = r.findAll("td")
+                scr.append(str(d))
+        count+=1
+    return scr  
 
-for fac in faclist:
-    if("index.php?id=" in fac):
-        faclist2.append((re.search('">.+<',fac).group()))
+def printfactor(data):
+    factmp = ''
+    for i,factor in enumerate(data):
+        if(i==2):
+            factmp = factor
+        
+    faclist = []
+    faclist = factmp.split("</a>")
+    faclist2 = []
+    for fac in faclist:
+        #print(fac)
+        if("index.php?id=" in fac):
+            if("..." in fac):
+                bigfactor(fac)
+            else:
+                faclist2.append((re.search('">.+<',fac).group()))
 
-for fact in faclist2:
-    fact = fact.replace('"><font color="#002099">','')
-    fact = fact.replace('"><font color="#000000">','')
-    print(fact.rstrip('<'))
+    for fact in faclist2:
+        fact = fact.replace('"><font color="#002099">','')
+        fact = fact.replace('"><font color="#000000">','')
+        #print()
+        print(fact.rstrip('<'))
 
+
+if __name__ == '__main__':
+    url = prepare()
+    soup = scraping(url)
+    tables = soup.findAll("table")
+    scr = find(tables)
+    data = scr[2].split(",")
+    print(data)
+    printfactor(data)
